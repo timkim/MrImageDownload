531eb2433008ac08bcf9cfdc34b60294c9848009
Tim Kim <tim.kim@nitobi.com>
2012-09-12 18:04:40 -0700

    Even more ddebugging

--- www/js/index.js
+++ www/js/index.js
@@ -29,8 +29,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        alert('ready');
         var theImage = document.getElementById('logo');
-        theImage.onclick = function(imgDownload){
+        theImage.onTouchEnd = function(imgDownload){
             MrImageDownload.download(theImage);
         }
     },
